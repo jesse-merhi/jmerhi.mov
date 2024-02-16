18466f6a180c9923f3d72c0080ef98e84ed7d517
jesse-merhi <jesse-merhi@users.noreply.github.com>
2024-02-16 02:42:24 +0000

    deploy: d0f50645d98aa5eb2fb1bf75c5d340f6cc03e843

--- 6443/reveal-js/plugin/math/katex.js
+++ 6443/reveal-js/plugin/math/katex.js
@@ -6,87 +6,91 @@
  * @author Gerhard Burger
  */
 export const KaTeX = () => {
-  let deck;
+	let deck;
 
-  let defaultOptions = {
-    version: "latest",
-    delimiters: [
-      { left: "$$", right: "$$", display: true }, // Note: $$ has to come before $
-      { left: "$", right: "$", display: false },
-      { left: "\\(", right: "\\)", display: false },
-      { left: "\\[", right: "\\]", display: true },
-    ],
-    ignoredTags: ["script", "noscript", "style", "textarea", "pre"],
-  };
+	let defaultOptions = {
+		version: 'latest',
+		delimiters: [
+			{left: '$$', right: '$$', display: true}, // Note: $$ has to come before $
+			{left: '$', right: '$', display: false},
+			{left: '\\(', right: '\\)', display: false},
+			{left: '\\[', right: '\\]', display: true}
+		],
+		ignoredTags: ['script', 'noscript', 'style', 'textarea', 'pre']
+	}
 
-  const loadCss = (src) => {
-    let link = document.createElement("link");
-    link.rel = "stylesheet";
-    link.href = src;
-    document.head.appendChild(link);
-  };
+	const loadCss = src => {
+		let link = document.createElement('link');
+		link.rel = 'stylesheet';
+		link.href = src;
+		document.head.appendChild(link);
+	};
 
-  /**
-   * Loads a JavaScript file and returns a Promise for when it is loaded
-   * Credits: https://aaronsmith.online/easily-load-an-external-script-using-javascript/
-   */
-  const loadScript = (src) => {
-    return new Promise((resolve, reject) => {
-      const script = document.createElement("script");
-      script.type = "text/javascript";
-      script.onload = resolve;
-      script.onerror = reject;
-      script.src = src;
-      document.head.append(script);
-    });
-  };
+	/**
+	 * Loads a JavaScript file and returns a Promise for when it is loaded
+	 * Credits: https://aaronsmith.online/easily-load-an-external-script-using-javascript/
+	 */
+	const loadScript = src => {
+		return new Promise((resolve, reject) => {
+			const script = document.createElement('script')
+			script.type = 'text/javascript'
+			script.onload = resolve
+			script.onerror = reject
+			script.src = src
+			document.head.append(script)
+		})
+	};
 
-  async function loadScripts(urls) {
-    for (const url of urls) {
-      await loadScript(url);
-    }
-  }
+	async function loadScripts(urls) {
+		for(const url of urls) {
+			await loadScript(url);
+		}
+	}
 
-  return {
-    id: "katex",
+	return {
+		id: 'katex',
 
-    init: function (reveal) {
-      deck = reveal;
+		init: function (reveal) {
 
-      let revealOptions = deck.getConfig().katex || {};
+			deck = reveal;
 
-      let options = { ...defaultOptions, ...revealOptions };
-      const { local, version, extensions, ...katexOptions } = options;
+			let revealOptions = deck.getConfig().katex || {};
 
-      let baseUrl = options.local || "https://cdn.jsdelivr.net/npm/katex";
-      let versionString = options.local ? "" : "@" + options.version;
+			let options = {...defaultOptions, ...revealOptions};
+			const {local, version, extensions, ...katexOptions} = options;
 
-      let cssUrl = baseUrl + versionString + "/dist/katex.min.css";
-      let katexUrl = baseUrl + versionString + "/dist/katex.min.js";
-      let mhchemUrl = baseUrl + versionString + "/dist/contrib/mhchem.min.js";
-      let karUrl = baseUrl + versionString + "/dist/contrib/auto-render.min.js";
+			let baseUrl = options.local || 'https://cdn.jsdelivr.net/npm/katex';
+			let versionString = options.local ? '' : '@' + options.version;
 
-      let katexScripts = [katexUrl];
-      if (options.extensions && options.extensions.includes("mhchem")) {
-        katexScripts.push(mhchemUrl);
-      }
-      katexScripts.push(karUrl);
+			let cssUrl = baseUrl + versionString + '/dist/katex.min.css';
+			let katexUrl = baseUrl + versionString + '/dist/katex.min.js';
+			let mhchemUrl = baseUrl + versionString + '/dist/contrib/mhchem.min.js'
+			let karUrl = baseUrl + versionString + '/dist/contrib/auto-render.min.js';
 
-      const renderMath = () => {
-        renderMathInElement(reveal.getSlidesElement(), katexOptions);
-        deck.layout();
-      };
+			let katexScripts = [katexUrl];
+			if(options.extensions && options.extensions.includes("mhchem")) {
+				katexScripts.push(mhchemUrl);
+			}
+			katexScripts.push(karUrl);
 
-      loadCss(cssUrl);
+			const renderMath = () => {
+				renderMathInElement(reveal.getSlidesElement(), katexOptions);
+				deck.layout();
+			}
 
-      // For some reason dynamically loading with defer attribute doesn't result in the expected behavior, the below code does
-      loadScripts(katexScripts).then(() => {
-        if (deck.isReady()) {
-          renderMath();
-        } else {
-          deck.on("ready", renderMath.bind(this));
-        }
-      });
-    },
-  };
+			loadCss(cssUrl);
+
+			// For some reason dynamically loading with defer attribute doesn't result in the expected behavior, the below code does
+			loadScripts(katexScripts).then(() => {
+				if( deck.isReady() ) {
+					renderMath();
+				}
+				else {
+					deck.on( 'ready', renderMath.bind( this ) );
+				}
+			});
+
+		}
+	}
+
 };
--- _app/immutable/entry/start.PoZI-OaL.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.CSQcHguz.js";export{t as start};
