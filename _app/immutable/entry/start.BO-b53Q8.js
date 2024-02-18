442bffd907001fec7dbfabff9d445733b4100170
jesse-merhi <jesse-merhi@users.noreply.github.com>
2024-02-18 10:32:02 +0000

    deploy: ea2c68e6308fb48602bbdc0ec175c3e855bde3a4

---
+++ _app/immutable/entry/start.BO-b53Q8.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.DBpNeotI.js";export{t as start};
--- functions/api/test.tsx
+++ functions/api/test.tsx
@@ -1,3 +1,30 @@
-export function onRequest(context) {
+export function onRequestPost(context) {
+  const fetch = require('node-fetch');
+  return new Response(`${context.request}`);
+  const url =
+    'https://gateway.ai.cloudflare.com/v1/0d425b8c2c85b36f347df36146f713ed/gpt-gateway/openai/chat/completions';
+  const accessToken = context.env.OPENAI_API;
+  const requestData = {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      model: 'gpt-3.5-turbo',
+      messages: [
+        {
+          role: 'user',
+          content: 'What is Cloudflare?',
+        },
+      ],
+    }),
+  };
+
+  fetch(url, requestData)
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error('Error:', error));
+
   return new Response(`This is a local environment! ${context.env.TEST}`);
 }
