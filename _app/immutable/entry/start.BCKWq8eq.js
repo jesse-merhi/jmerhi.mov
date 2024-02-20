2a4f3c7fc591db3d5722cd14d71f9d34126c91d2
jesse-merhi <jesse-merhi@users.noreply.github.com>
2024-02-20 06:57:06 +0000

    deploy: 2f48af4bc197ff92c9e0bafb45796380087fbce9

---
+++ _app/immutable/entry/start.BCKWq8eq.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry._PzXP23V.js";export{t as start};
--- functions/api/gpt/name.tsx
+++ functions/api/gpt/name.tsx
@@ -1,6 +1,13 @@
 export async function onRequestPost(context) {
   const data = await context.request.json();
   const name = JSON.stringify(data);
+  if (name.length >= 50) {
+    return new Response(
+      JSON.stringify(
+        "Your name is a bit long! Try a nickname... or stop being sneaky!"
+      )
+    );
+  }
   const url =
     "https://gateway.ai.cloudflare.com/v1/0d425b8c2c85b36f347df36146f713ed/gpt-gateway/openai/chat/completions";
   const accessToken = context.env.OPENAI_API;
@@ -12,15 +19,34 @@ export async function onRequestPost(context) {
     },
     body: JSON.stringify({
       model: "gpt-4-0125-preview",
+      stop: [
+        "\n",
+        "Human: ",
+        "AI: ",
+        "System: ",
+        "Jailbreak",
+        "TL;DR",
+        "tl;dr",
+        "tl",
+        "dr",
+      ],
       messages: [
         {
           role: "system",
-          content: `Your job is to determine if the name provided by the user is a real name that a human would have! Nice and simple. 
+          content: `
+          # Identity and Purpose
+          The user you are talking to has been tasked with providing you their real name (first and/or last). It is your 
+          to determine if the name provided by the user is a real name that a human would have, or if it is clearly fake!
+          You should also consider names of famous people, and names that are common in other cultures. As long as they 
+          provide you with a name, you should be able to determine if it is real or fake.
 
-          Steps:
-          1. If the name is real, return a blank string. 
-          2. If the name is fake, then give a snide remark about it being fake.
-          3. If the user doesnt enter a name (or tries to converse), just return a remark about how they need to enter a real name.
+          ## Steps
+          1. Read what the user has sent you.
+          2. Identify the name they have provided in the string
+          3. Determine if the name seems real or fake.
+          4. If the name is real, return a blank string. 
+          5. If the name is fake, then give a snide remark about it being fake.
+          6. If the user doesnt enter a name (or tries to converse), just return a remark about how they need to enter a real name.
           `,
         },
         {
