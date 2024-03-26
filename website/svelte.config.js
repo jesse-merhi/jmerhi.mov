88c9558f2901708945decb5ee4b29949e61dc65a
Jesse Merhi <79823012+jessemerhi1@users.noreply.github.com>
2024-03-27 08:56:06 +1100

    STATIC

--- website/svelte.config.js
+++ website/svelte.config.js
@@ -1,5 +1,5 @@
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,7 +13,7 @@ const config = {
 		}),
 	  },
 
-    preprocess: [vitePreprocess({})]
+    preprocess: vitePreprocess()
 };
 
 export default config;
