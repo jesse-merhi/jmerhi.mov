d05a2c8d7d63020d026c17b43b696c8179e7967d
Jesse Merhi <79823012+jessemerhi1@users.noreply.github.com>
2024-05-10 23:35:45 +1000

    no more loading

--- website/src/routes/blog/[blog_name]/+page.svelte
+++ website/src/routes/blog/[blog_name]/+page.svelte
@@ -19,16 +19,21 @@
   });
 </script>
 
-<div class="h-[100%] w-[95%] sm:w-[70%]">
-  <div
-    class="markdown h-[100%] w-[100%] pr-[20px] pl-[20px] pb-[50px] overflow-y-scroll overflow-x-hidden break-words"
-  >
-    <div class="h-[50px]"></div>
-    <h1>{blogConfig.title || ""}</h1>
-    <p>{blogConfig.author || ""} {blogConfig.published || ""}</p>
-    <SvelteMarkdown source={blogContent} renderers={{ code: CodeComponent }} />
+{#if blogConfig != {}}
+  <div class="h-[100%] w-[95%] sm:w-[70%]">
+    <div
+      class="markdown h-[100%] w-[100%] pr-[20px] pl-[20px] pb-[50px] overflow-y-scroll overflow-x-hidden break-words"
+    >
+      <div class="h-[50px]"></div>
+      <h1>{blogConfig.title || ""}</h1>
+      <p>{blogConfig.author || ""} {blogConfig.published || ""}</p>
+      <SvelteMarkdown
+        source={blogContent}
+        renderers={{ code: CodeComponent }}
+      />
+    </div>
   </div>
-</div>
+{/if}
 
 <style lang="postcss">
   * {
