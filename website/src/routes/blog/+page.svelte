121535e778fb5f0ac9de312b2404ed6c31a7ddb2
Jesse Merhi <79823012+jessemerhi1@users.noreply.github.com>
2024-05-11 00:44:09 +1000

    mobile dev confirmed

--- website/src/routes/blog/+page.svelte
+++ website/src/routes/blog/+page.svelte
@@ -11,21 +11,38 @@
   ];
 </script>
 
-<div class="h-[100%] w-[80%] pt-10">
+<div
+  class="h-[100%] w-[90%] sm:w-[80%] md:w-[70%] min-[1024px]:w-[60%] pt-10 overflow-y-auto flex flex-col items-center"
+>
   {#each blogs as blog}
-    <div class="h-[22rem] w-[28rem] border-2 border-border">
-      <a href={`/blog/${blog.path}`}>
-        <div class="w-full h-[60%] overflow-hidden">
-          <img src="/imgs/Auth.webp" alt="images" />
+    <div
+      class=" w-full sm:w-full border-2 border-border mb-10 sm:max-h-[30%] hover:border-muted-foreground transition-all ease-in-out duration-200"
+    >
+      <a
+        href={`/blog/${blog.path}`}
+        class="w-full h-full flex flex-col sm:flex-row"
+      >
+        <div class="w-full h-[10rem] sm:h-full sm:w-[30%] overflow-clip">
+          <img
+            src="/imgs/Auth.webp"
+            class="h-full w-full object-cover"
+            alt="images"
+          />
         </div>
-        <div class="flex flex-col h-[40%] justify-center p-5">
-          <div class="text-foreground font-extrabold text-xl">
+        <div class="flex flex-col justify-center p-5 sm:w-[70%]">
+          <div
+            class="text-foreground font-extrabold text-xl min-[1150px]:text-2xl"
+          >
             {blog.title}
           </div>
-          <div class="text-muted-foreground font-light font-bold text-xs p-y-2">
+          <div
+            class="text-muted-foreground font-light font-bold text-xs p-y-2 min-[1150px]:text-sm"
+          >
             {blog.author} | {blog.published}
           </div>
-          <div class="text-sm text-ellipsis overflow-hidden">
+          <div
+            class=" text-xs sm:text-xs md:text-sm min-[1150px]:text-lg text-ellipsis overflow-hidden"
+          >
             AI and LLMs are the two biggest buzzwords right now and for the most
             part they are relatively secure. But how does that translate into
             the future? Should we be worried about the future of AI?
