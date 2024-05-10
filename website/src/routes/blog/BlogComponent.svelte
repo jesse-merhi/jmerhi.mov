850d3e85358b42504c1af2e0c7fd739f87c8ebf5
Jesse Merhi <79823012+jessemerhi1@users.noreply.github.com>
2024-05-10 15:14:47 +1000

    blog content

--- website/src/routes/blog/BlogComponent.svelte
+++ website/src/routes/blog/BlogComponent.svelte
@@ -1,7 +1,7 @@
 <script>
   import SvelteMarkdown from "svelte-markdown";
   import CodeComponent from "./CodeComponent.svelte";
-  export let blogContent;
+  export let blogContent = "";
   export let blogConfig;
 </script>
 
@@ -13,7 +13,7 @@
     <h1>{blogConfig.title || ""}</h1>
     <p>{blogConfig.author || ""} {blogConfig.published || ""}</p>
     <!-- <SvelteMarkdown source={blogContent} renderers={{ code: CodeComponent }} /> -->
-    <!-- <SvelteMarkdown source={blogContent} /> -->
+    <SvelteMarkdown source={blogContent} />
   </div>
 </div>
 
